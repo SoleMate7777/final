bc554f654c224c3a88db5a97c14ec76e0e030d76
SoleMate7777 <103522512+SoleMate7777@users.noreply.github.com>
2023-06-02 17:03:15 -0400

    Add files via upload

---
+++ COUNT.py
@@ -0,0 +1,17 @@
+import mysql.connector as mc
+conn = mc.connect(host='localhost', user='Chris', password='C123456789.', db='menagerie')
+c = conn.cursor()
+
+
+def COUNT():
+    c.execute('SELECT owner, COUNT(*) FROM pet GROUP BY owner')
+    pets = c.fetchall()
+    for pet in pets:
+        print(pet)
+
+
+def main():
+    COUNT()
+
+if __name__ == '__main__':
+    main()
---
+++ name_birth.py
@@ -0,0 +1,17 @@
+import mysql.connector as mc
+conn = mc.connect(host='localhost', user='Chris', password='C123456789.', db='menagerie')
+c = conn.cursor()
+
+
+def show_data():
+    c.execute('SELECT name, birth FROM pet')
+    pets = c.fetchall()
+    for pet in pets:
+        print(pet)
+
+
+def main():
+    show_data()
+
+if __name__ == '__main__':
+    main()
